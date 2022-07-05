bce41b878f36ffb96e0acd9d7e001a5e1194ee2a
CKraft11 <cadenkraft@gmail.com>
2022-07-05 18:22:38 -0500

    websocket connection

--- js/telem.js
+++ js/telem.js
@@ -1,5 +1,11 @@
 var telem = new WebSocket("ws://telem.ctrtl.com/ws");
 
+telem.onopen = function(e) {
+  alert("[open] Connection established");
+  alert("Sending to server");
+  telem.send("P15 Telem Connection Established");
+};
+
 telem.onmessage = function (event) {
     console.log(event.data);
   }
