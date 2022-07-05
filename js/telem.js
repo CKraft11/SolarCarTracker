a9929314b6a3de748bc9c9585ef462ed5db34349
CKraft11 <cadenkraft@gmail.com>
2022-07-05 18:45:59 -0500

    minor websocket subscription updates

--- js/telem.js
+++ js/telem.js
@@ -3,12 +3,12 @@ var telem = new WebSocket("ws://telem.ctrtl.com/ws");
 telem.onopen = () => {
   alert("[open] Connection established");
   alert("Sending to server");
-  const msg = {
+  const strategy = {
     type: 'subscribe',
     channel: '4',
     interval: 500
   };
-  telem.send(JSON.stringify(msg));
+  telem.send(JSON.stringify(strategy));
 };
 telem.onmessage = function (event) {
     console.log(event.data);
