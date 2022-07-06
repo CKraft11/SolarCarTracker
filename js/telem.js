913a5457ba9313c4ae1094cf998ba9a80546166b
CKraft11 <cadenkraft@gmail.com>
2022-07-05 20:03:04 -0500

    remove int32 function

--- js/telem.js
+++ js/telem.js
@@ -1,22 +1,11 @@
 var telem = new WebSocket("ws://telem.ctrtl.com/ws");
 
-function ToInt32(x) {
-  var uint32 = ToUint32(x);
-  if (uint32 >= Math.pow(2, 31)) {
-      return uint32 - Math.pow(2, 32)
-  } else {
-      return uint32;
-  }
-}
-channelNum = ToInt32(4)
-
 telem.onopen = () => {
   alert("[open] Connection established");
   alert("Sending to server");
   const strategy = {
     type: 'subscribe',
-    channel: channelNum,
-    interval: 1
+    channel: 0x4
   };
   telem.send(JSON.stringify(strategy));
 };
