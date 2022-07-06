ce7414ca9620a7ea8b6bc63fceeb0f128aa5af3f
Electrollium <69403267+Electrollium@users.noreply.github.com>
2022-07-05 22:51:01 -0500

    animated track update

--- js/script.js
+++ js/script.js
@@ -13,15 +13,14 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-function onEachFeature(feature, layer) {
-  layer.bindPopup(feature.properties.nazwa);
-}
+//finishline
+L.marker([42.5839, -114.4710], {icon: finishIcon}).addTo(map);
 
-L.marker(endLocation, {icon: finishIcon}).addTo(map);
-//var finishline = new L.Marker([42.5839, -114.4710]);
-L.marker([solarLat, solarLong], {icon: p15Icon}).addTo(map);
-//var solarcar = new L.Marker([solarLat, solarLong],solarDetails);
-//solarcar.addTo(map);
+//carmarker
+var carMarker = L.marker([solarLat, solarLong], {icon: p15Icon}).addTo(map);
+
+var tracks = [];
+
 // adding geojson by fetch
 // of course you can use jquery, axios etc.
 fetch("asc.json")
@@ -31,6 +30,35 @@ fetch("asc.json")
   .then(function (data) {
     // use geoJSON
     L.geoJSON(data, {
-      onEachFeature: onEachFeature,
-    }).addTo(map);
+		onEachFeature: onEachFeature,
+		}).addTo(map);
+	
+	//console.log(tracks);
+	setInterval(updateTrackAnimation, 1000/20);//20fps updates
   });
+  
+ function onEachFeature(feature, layer) {
+	layer.bindPopup(feature.properties.nazwa);
+	console.log(feature);
+	  
+	if(feature.geometry.type == "LineString"){  
+	  
+		layer.setStyle({dashArray:"10 10", 
+						dashOffset:0,
+						color:"#88120d"});
+	  
+		tracks.push(layer); //add to tracks list
+	}
+	if(feature.geometry.type == "Point"){
+	
+	}	
+}
+
+function updateTrackAnimation(){
+	for(const key in tracks){
+		
+		//console.log(tracks[key]);
+		let newOffset = (tracks[key].options.dashOffset-1) % 20;
+		tracks[key].setStyle({dashOffset:newOffset});
+	}
+}
