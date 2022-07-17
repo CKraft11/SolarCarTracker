c957949f04b215d93445457cf6ad9a4111b71216
Caden Kraft <58564269+CKraft11@users.noreply.github.com>
2022-07-17 13:59:43 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,48 +24,51 @@ console.log(terrain)
 // Used to load and display tile layers on the map
 // Most tile servers require attribution, which you can set under `Layer`
 //finishline
-L.marker([42.5839, -114.4710], {icon: finishIcon}).addTo(map);
+
 
 //carmarker
 var carMarker = L.marker([solarLat, solarLong], {icon: p15Icon});
 var carMarkerF = L.marker([solarLat, solarLong], {icon: p15fIcon});
 
-var tracks = [];
+var showRoute = false;
 
-// adding geojson by fetch
-// of course you can use jquery, axios etc.
-fetch("asc.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    // use geoJSON
-    L.geoJSON(data, {
+if(showRoute==true){
+	L.marker([42.5839, -114.4710], {icon: finishIcon}).addTo(map);
+	// adding geojson by fetch
+	// of course you can use jquery, axios etc.
+	fetch("asc.json")
+  	.then(function (response) {
+    	return response.json();
+  	})
+  	.then(function (data) {
+    	// use geoJSON
+    	L.geoJSON(data, {
 		onEachFeature: onEachFeature,
 		}).addTo(map);
 	
 	//console.log(tracks);
 	setInterval(updateTrackAnimation, 1000/20);//20fps updates
-  });
+  	});
   
- function onEachFeature(feature, layer) {
+ 	function onEachFeature(feature, layer) {
 	//layer.bindPopup(feature.properties.nazwa);
 	//console.log(feature);
 	  
-	if(feature.geometry.type == "LineString"){  
+		if(feature.geometry.type == "LineString"){  
 	  
-		layer.setStyle({dashArray:"10 10", 
+			layer.setStyle({dashArray:"10 10", 
 						dashOffset:0,
 						color:"#0278fc"});
 	  
 		tracks.push(layer); //add to tracks list
-	}
-	if(feature.geometry.type == "Point"){
+		}
+		if(feature.geometry.type == "Point"){
 		
 		//console.log(feature);
-    //layer.bindTooltip(feature.properties.name,{direction:"top"});
+    		//layer.bindTooltip(feature.properties.name,{direction:"top"});
 		layer.bindTooltip(feature.properties.name,{sticky:true,direction:"top"});
-	}	
+		}	
+	}
 }
 
 function updateTrackAnimation(){
